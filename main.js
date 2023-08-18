b48c5303c3f2799a0d5096e16f6dba761dcd63c3
Jyoti Vaishnav <jyoti.vaishnav666@gmail.com>
2023-08-18 18:27:42 -0400

    add html,css,js files

---
+++ main.js
@@ -0,0 +1,21 @@
+    /*----- constants -----*/
+
+
+	/*----- state variables -----*/
+
+
+	/*----- cached elements  -----*/
+
+
+	/*----- event listeners -----*/
+
+
+	/*----- functions -----*/
+    init();
+    //Initialize all state, then call render();
+    function init(){
+
+
+
+      render();
+    } 
