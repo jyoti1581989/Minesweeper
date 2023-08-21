54bc39ee30d6c5cb943ba08fb48f8bd284d8dc17
Jyoti Vaishnav <jyoti.vaishnav666@gmail.com>
2023-08-21 00:02:45 -0400

    created grid and wrote function to set mines

--- js/main.js
+++ js/main.js
@@ -2,23 +2,72 @@
     const ROW_SIZE = 8;
     const COL_SIZE = 8;
     const GRID_SIZE = ROW_SIZE*COL_SIZE;
+    const MINE_COUNT = (GRID_SIZE)/5;
 	/*----- state variables -----*/
-   let gridArray ;
-     
-
-	/*----- cached elements  -----*/
+   let gridObjArray ;
+  
+   
+  /*----- cached elements  -----*/
+  const uiGrid = document.getElementById('board');
 
 
 	/*----- event listeners -----*/
+
+
+
+  /*------Class----------*/
+  class Cell{
+    constructor(row, col, isMined){
+      this.row = row
+      this.col = col
+      this.isMined = isMined // boolean to determine if cell is mined or not
+      this.content = ''
+    }
+
+    mined(){
+      return this.isMined
+    }
+
+    placeMine(){
+      this.isMined = true
+      this.content = `<img src='images/bomb.png'/>`
+    }
+  }  
 
 
 	/*----- functions -----*/
     init();
     //Initialize all state, then call render();
     function init(){
-      gridArray = [];
-
-
-
+      gridObjArray = [];
+      for(let i = 0;i < ROW_SIZE;i++){
+        gridObjArray[i] = []
+        for(let j =0;j < COL_SIZE;j++){
+          gridObjArray[i][j] = new Cell(i, j, false);
+        }
+      }
+      setMines();
       render();
-    } 
+    }
+    // Places mines(20% of grid size) in the grid 
+
+    function setMines(){
+      let mineCnt = MINE_COUNT
+      let r=0, c=0;
+      let randomCell
+      while(mineCnt > 0){
+        randomCell = Math.floor(Math.random()*GRID_SIZE)
+        r = Math.floor(randomCell/ROW_SIZE)
+        c = randomCell%COL_SIZE
+        if(!gridObjArray[r][c].mined()){
+          gridObjArray[r][c].placeMine()
+          mineCnt--
+        
+        }
+
+      }
+    }
+
+
+      
+     
